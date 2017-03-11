13fee20902917a593a02b77f197167b48d38accc
Samuel W Yerkes <syerkes@vcu.edu>
2017-03-10 22:54:24 -0500

    Added style to register page.

--- resources/views/snippets/index.blade.php
+++ resources/views/snippets/index.blade.php
@@ -1,7 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-	@foreach($snippets as $snippet)
-		<h1><a href="{{ route('home.show', $snippet->id) }}">{{ $snippet->title }}</a></h1>
-	@endforeach
+	@if($snippets->count())
+		@foreach($snippets as $snippet)
+			<h1><a href="{{ route('home.show', $snippet->id) }}">{{ $snippet->title }}</a></h1>
+		@endforeach
+	@else
+		<p class="has-text-centered">Oh no! Looks like you don't have any snippets yet.</p>
+	@endif
 @endsection
