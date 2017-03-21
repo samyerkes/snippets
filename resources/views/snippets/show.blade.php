3c74d6c4a9493c7eedad1075495912cba5e28bd4
Samuel W Yerkes <syerkes@vcu.edu>
2017-03-20 20:03:37 -0400

    Added heading to snippet/show

--- resources/views/snippets/show.blade.php
+++ resources/views/snippets/show.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-	<a href="{{ route('snippets.edit', $snippet->id) }}">Edit</a>
+	<div class="columns">
+		<h1 class="column is-11">{{ $snippet->title }}</h1>
+		<a class="column is-1" href="{{ route('snippets.edit', $snippet->id) }}">Edit</a>
+	</div>
 	<snippet :id="{{ $snippet->id }}" :message="'{{ $snippet->body }}'" :token="'{{ Auth::user()->api_token }}'"></snippet>
 @endsection
